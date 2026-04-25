ad2b821c4b8dd7a0ccf12584eda96e0096d1b422
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2026-04-25 17:20:35 +0500

    interfaces in domain

---
+++ FreeRoom/backend/src/Domain/Interfaces/IRoomDynamicRepository.cs
@@ -0,0 +1,19 @@
+﻿using FreeRoom.backend.src.Domain.Enities;
+
+namespace FreeRoom.backend.src.Domain.Interfaces;
+
+public interface IRoomDynamicRepository
+{
+    Task<List<RoomDynamic>> GetBySubjectId(Guid subjectId);
+    Task<List<RoomDynamic>> GetAll();
+    Task<RoomDynamic?> GetByNameRoomDynamic(string nameRoomDynamic);
+    Task<bool> DeleteRoomDynamic(Guid id);
+    Task<RoomDynamic?> GetByIdRoomDynamic(Guid id);
+    Task<RoomDynamic> CreateRoomDynamic(RoomDynamic material);
+    
+    Task<RoomDynamic?> UpdateRoomDynamic(RoomDynamic material);
+    Task<List<RoomDynamic>> SearchAsync(string searchText);
+    
+    Task<bool> IncrementViewCountAsync(Guid id);
+    Task<bool> IncrementDownloadCountAsync(Guid id);
+}
---
+++ FreeRoom/backend/src/Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using FreeRoom.backend.src.Domain.Enities;
+
+namespace FreeRoom.backend.src.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByLoginAsync(string login, CancellationToken cancellationToken = default);
+    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task AddAsync(User user, CancellationToken cancellationToken = default);
+    Task UpdateAsync(User user, CancellationToken cancellationToken = default);
+}
